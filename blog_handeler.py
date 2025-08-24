6cdb83bc5eb1f5297fb3325bcbc53cef424006f0
anchored-archer <wandering-sandstorm@proton.me>
2025-08-24 23:04:44 +0530

    Implemented A way to check if files are modified

--- blog_handeler.py
+++ blog_handeler.py
@@ -2,15 +2,44 @@ import os
 import frontmatter
 import time
 import mistune
-import json
 import sqlite3
 
-def fetch(database):
-    con = sqlite3.connect("database.db")
-    cur = con.cursor()
+def check_modfied(cur, filepath) -> list | None:
+    """
+    Given A filepath, and a database handeling object, check's if any modfication of file in the said path, is registrered to the db.
+    """
+    # Retrive L.M.D's from database 
+    cur.execute("SELECT last_modified_date FROM records;")
+    last_modfified_dates_db : list = [row[0] for row in cur.fetchall()]
 
-    last_modfified_dates = cur.execute("SELECT last_modified_date FROM records;")
-    #WIP
+    # Retrive L.M.D's from os
+    last_modfified_dates_os : list = []
+    os.chdir(filepath)
+    directories : list = os.listdir()
+    for filename in directories:
+        timestamp = os.path.getmtime(filename)
+        last_modified_date: str = time.strftime('%B %d %Y', time.localtime(timestamp))
+        last_modfified_dates_os.append(last_modified_date)
+    changed_LMDs : list = []
+    
+    # Check if the os's LMD databases's LMD match
+    for last_modfified_date_db, last_modfified_date_os in zip(last_modfified_dates_db, last_modfified_dates_os):
+        if last_modfified_date_db != last_modfified_date_os:
+            changed_LMDs.append(last_modfified_date_os)
+    
+    # pulls up, and returns a list of filenames, whose lmd's where modfified; If no mod, returns None
+    if len(changed_LMDs) != 0:
+        filenames : list = []
+        for filename, changed_date in zip(directories, changed_LMDs): 
+            timestamp = os.path.getmtime(filename)
+            last_modified_date : str = time.strftime('%B %d %Y', time.localtime(timestamp))
+
+            if last_modified_date == changed_date:
+                filenames.append(filename.rstrip('.md'))
+        
+        return filenames 
+    else:
+        return None
 
 def convert_mdh(text: str) -> str:
     """ Converts a markdown into HTML """
@@ -22,7 +51,6 @@ class Database():
     def __init__(self, filepath = "C:/Users/ghosa/Desktop/Master Folder/Programing/portfolio-web/blog") -> None:
         self.cur = None
         self.filepath = filepath
-
     
     def create_db(self):
         # Create SQLite Database
@@ -53,8 +81,10 @@ class Database():
             except sqlite3.IntegrityError:
                 continue
             self.con.commit()
+        
+        return self.cur, absolute_blog_folder_path
 
 # Create Database
 database = Database()
-database.create_db()
+cur, filepath = database.create_db()
 
