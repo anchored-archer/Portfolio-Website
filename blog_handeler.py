2034ce70ff81b55f2ae5379c041f96e204c34ab8
anchored-archer <wandering-sandstorm@proton.me>
2025-08-24 23:04:44 +0530

    Removed unnecessary shenanigans

--- blog_handeler.py
+++ blog_handeler.py
@@ -11,45 +11,8 @@ def convert_mdh(text: str) -> str:
     html = markdown(text)
     return html
 
-class Metadata():
-    def __init__(self, title : str, metapath: str =  "C:/Users/ghosa/Desktop/Master Folder/Programing/portfolio-web/metadata") -> None:
-        self.metapath = metapath
-        self.title = title
-    
-    def pull_metadata(self) -> bool | Exception:
-        """
-        Pulls up the given metdata from the given filepath; returns error if unsucessful
-        """
-        self.metadata_path = f"{self.metapath}/{self.title}.json"
-        try:
-            with open(self.metadata_path, 'r') as metafile:
-                metadata_str = metafile.read()
-                metadata = json.loads(metadata_str)
-            return metadata['accessed']
-        
-        except FileNotFoundError:
-            raise FileNotFoundError
-        
-    def write_metadata(self, metadata: dict) -> bool | Exception:
-        """
-        Writes the given metadata to a ads_path --- 
-        a path that constitues of the filepath to the object, 
-        and the name of the variable in which metadata is stored in a string format. 
-        "metadata" is the dictionary "contaning" the key, value pairs of the metadata that has to be written 
-        """
-        self.metadata_path = f"{self.metapath}/{self.title}.json"
-        metadata_string = json.dumps(metadata)
-        try:
-            with open(self.metadata_path, 'w') as ads_file:
-                ads_file.write(metadata_string)
-            return metadata["accessed"]
-        except:
-            raise 
-
-class Database(Metadata):
+class Database():
     def __init__(self, filepath = "C:/Users/ghosa/Desktop/Master Folder/Programing/portfolio-web/blog") -> None:
-        super().__init__(title=None)
-        self.con = None
         self.cur = None
         self.filepath = filepath
 
@@ -58,7 +21,7 @@ class Database(Metadata):
         # Create SQLite Database
         self.con = sqlite3.connect("database.db")
         self.cur = self.con.cursor()
-        self.cur.execute("CREATE TABLE IF NOT EXISTS records (title TEXT NOT NULL, data TEXT NOT NULL, last_modified_date TEXT NOT NULL, accessed BOOLEAN NOT NULL);")
+        self.cur.execute("CREATE TABLE IF NOT EXISTS records (title TEXT NOT NULL, data TEXT NOT NULL, last_modified_date TEXT NOT NULL);")
 
         # Intiate Search for written blogs
         absolute_blog_folder_path = self.filepath
@@ -76,15 +39,9 @@ class Database(Metadata):
             title = filename.rstrip('.md')
             self.title = title
             last_modified_date = time.strftime('%B %d %Y', time.localtime(timestamp))
-
-            try: 
-                accessed = self.pull_metadata()
-            except FileNotFoundError:
-                metadata = {"accessed": False}
-                accessed = self.write_metadata(metadata)
             
             # Fill Database
-            self.cur.execute("INSERT INTO records (title, data, last_modified_date, accessed) VALUES (?,?,?,?);", (title, data, last_modified_date, accessed))
+            self.cur.execute("INSERT INTO records (title, data, last_modified_date) VALUES (?,?,?);", (title, data, last_modified_date))
             self.con.commit()
 
 def check_changes():
